cd014897aa5f04e2d7c31042b7d6f568b169200a
OlesiaPonomarenko <160940182+OlesiaPonomarenko@users.noreply.github.com>
2025-12-29 14:08:19 +0200

    new

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function christmasCountdown () {
-    const christmasDay = (new Date('December 25 2025 00:00'));
+    const christmasDay = (new Date('December 25 2026 00:00'));
     const now = new Date();
     const diff = christmasDay - now;
     const msInSecond = 1000;
